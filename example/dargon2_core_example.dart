74f1b15737720e5eb47fde4f6057f56be69330f6
Tejas Mehta <tmthecoder@gmail.com>
2021-06-02 10:42:47 -0400

    add an example

--- example/dargon2_core_example.dart
+++ example/dargon2_core_example.dart
@@ -1,6 +1,22 @@
+import 'dart:ffi';
+
 import 'package:dargon2_core/dargon2_core.dart';
 
 void main() {
-  // var awesome = Awesome();
-  // print('awesome: ${awesome.isAwesome}');
+  // Create an instance of DArgon2
+  final argon2 = DArgon2(TestLibLoader());
+}
+
+class TestLibLoader implements LibLoader {
+  @override
+  String getPath() {
+    // Return the Argon2 Reference Library's path here
+    throw UnimplementedError();
+  }
+
+  @override
+  DynamicLibrary loadLib() {
+    // Return the actual loaded DynamicLibary here
+    throw UnimplementedError();
+  }
 }
